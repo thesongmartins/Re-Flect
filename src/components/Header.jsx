fc0f1e501aa3858eb25a62c87060566045080066
thesongmartins <martinsjehoshaphat@gmail.com>
2024-12-03 00:42:45 +0100

    Fixing Tailwind Problem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 const Header = () => {
   return (
     <>
-      <div className="text-red-600">Header</div>
+      <div className="text-red-600 bg-blue-600">Header</div>
       <div>
         <Button text="Get Started" href="/login" color="Blue" />
       </div>
