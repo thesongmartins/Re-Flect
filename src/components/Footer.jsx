53511b0f7eb1831128801b1e9e61511049179ade
thesongmartins <martinsjehoshaphat@gmail.com>
2024-12-03 01:22:26 +0100

    Creating Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,37 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="flex justify-between p-10 bg-gray-200">
+        <p className="text-bold text-xl">Re-Flect</p>
+        <div>
+          <ul className="flex gap-10">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Profile</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          {/* <FontAwesomeIcon icon="fa-brands fa-x-twitter" />
+          <FontAwesomeIcon icon="fa-brands fa-instagram" />
+          <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+          <FontAwesomeIcon icon="fa-brands fa-facebook" /> */}
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
