2b524ddf49db450bbab3ed5768d05d5a56e84c38
thesongmartins <martinsjehoshaphat@gmail.com>
2024-12-05 04:29:22 +0100

    Updating how it works section

--- src/pages/Home/HowItWorks.jsx
+++ src/pages/Home/HowItWorks.jsx
@@ -7,28 +7,28 @@ const HowItWorks = () => {
           Reflect on your day, emotions, and thoughts with guided prompts or
           freeform entries
         </p>
-        <div className="flex bg-gray-300 rounded-lg p-10  gap-3">
-          <div className="border border-blue-300 rounded p-5">
+        <div className="flex rounded-lg p-10 gap-3">
+          <div className="border border-blue-300 rounded p-10">
             <img src="" alt="" />
-            <h2>Sign Up</h2>
+            <h2 className="font-bold">Sign Up</h2>
             <p>
               Create an account by signing up with your email. Set your
               preferences to receive guided prompts tailored to your unique
               needs and goals.
             </p>
           </div>
-          <div className="border border-blue-300 rounded p-5">
+          <div className="border border-blue-300 rounded p-10">
             <img src="" alt="" />
-            <h2>Choose Entry Type</h2>
+            <h2 className="font-bold">Choose Entry Type</h2>
             <p>
               Let our guided prompts inspire deeper reflection or freely express
               your thoughts with unstructured entries.The choice is yours,every
               time.
             </p>
           </div>
-          <div className="border border-blue-300 rounded p-5">
+          <div className="border border-blue-300 rounded p-10">
             <img src="" alt="" />
-            <h2>Reflect and Track</h2>
+            <h2 className="font-bold">Reflect and Track</h2>
             <p>
               Log your daily moods and track your emotional pattern overtime.
               Easily revisit past entries to see how fast you have come on your
