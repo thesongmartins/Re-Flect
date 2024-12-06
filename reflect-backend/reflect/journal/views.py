ba1315f48ab1f31b2289aa8ac53127531f647515
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-12-06 07:05:36 +0000

    create views for the journal app

--- reflect-backend/reflect/journal/views.py
+++ reflect-backend/reflect/journal/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import JournalEntry, Prompt, Tag, Mood
+from .serializers import JournalEntrySerializer, PromptSerializer, TagSerializer, MoodSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class JournalEntryViewSet(viewsets.ModelViewSet):
+    queryset = JournalEntry.objects.all()
+    serializer_class = JournalEntrySerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PromptViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Prompt.objects.all()
+    serializer_class = PromptSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class MoodViewSet(viewsets.ModelViewSet):
+    queryset = Mood.objects.all()
+    serializer_class = MoodSerializer
