9ab9736fa68a6c564414562bfd76c8ed31e1d6a0
thesongmartins <martinsjehoshaphat@gmail.com>
2024-12-06 20:16:42 +0100

    Updating UI components

--- src/pages/Home/Features.jsx
+++ src/pages/Home/Features.jsx
@@ -4,9 +4,12 @@ const Features = () => {
   // console.log(features);
   return (
     <>
-      <div className="flex ">
+      <div className="">
         {features.map((feature) => (
-          <div key={feature.id}>
+          <div
+            key={feature.id}
+            className="flex align-center items-center gap-5 font-bold text-blue-500 odd:flex-row-reverse"
+          >
             <h5>{feature.title}</h5>
             <p>{feature.description}</p>
             <img src={feature.img} alt={feature.title} />
