f2e2adfffa8d8b1fbdc6004f63ea083ef2ed42e4
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-12-06 05:10:43 +0000

    create the user model (use Django built in AbstractUsers to allow easy customization of the user model)

--- reflect-backend/reflect/users/models.py
+++ reflect-backend/reflect/users/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
