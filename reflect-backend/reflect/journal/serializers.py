f7e1d40362c9e412e3880d7b118abf5a989023ee
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-12-28 14:53:17 +0000

    remove sentiment analysis and creatserializers l for basic functionalities

--- reflect-backend/reflect/journal/serializers.py
+++ reflect-backend/reflect/journal/serializers.py
@@ -1,22 +1,10 @@
 from rest_framework import serializers
-from .models import JournalEntry, Media, Tag, EntryTag, Mood
-
-class MediaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Media
-        fields = ['id', 'file', 'uploaded_at']
+from .models import JournalEntry, Tag, EntryTag, Mood
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ['id', 'name']
-
-class EntryTagSerializer(serializers.ModelSerializer):
-    tag = TagSerializer()
-
-    class Meta:
-        model = EntryTag
-        fields = ['id', 'tag']
 
 class MoodSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,14 +12,25 @@ class MoodSerializer(serializers.ModelSerializer):
         fields = ['id', 'mood_type', 'mood_score', 'timestamp']
 
 class JournalEntrySerializer(serializers.ModelSerializer):
-    media = MediaSerializer(many=True, read_only=True)
-    tags = EntryTagSerializer(many=True, read_only=True)
+    tags = serializers.ListField(
+        child=serializers.CharField(max_length=50),
+        write_only=True,
+        required=False
+    )
     moods = MoodSerializer(many=True, read_only=True)
 
     class Meta:
         model = JournalEntry
         fields = [
-            'id', 'user', 'title', 'content', 'created_at', 'updated_at', 
-            'sentiment_score', 'sentiment_label', 'media', 'tags', 'moods'
+            'id', 'user', 'title', 'content', 'photo', 'video', 'audio',
+            'created_at', 'updated_at', 'tags', 'moods'
         ]
-        read_only_fields = ['user', 'sentiment_score', 'sentiment_label']
+        read_only_fields = ['user']
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags', [])
+        journal_entry = JournalEntry.objects.create(**validated_data)
+        for tag_name in tags:
+            tag, _ = Tag.objects.get_or_create(user=self.context['request'].user, name=tag_name)
+            EntryTag.objects.create(journal_entry=journal_entry, tag=tag)
+        return journal_entry
