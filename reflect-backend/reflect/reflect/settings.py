10ad3c7b9db83ba0bbb335cf6dc1e8297454ba14
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-12-28 15:39:53 +0000

    update settings and main project urls

--- reflect-backend/reflect/reflect/settings.py
+++ reflect-backend/reflect/reflect/settings.py
@@ -109,6 +109,9 @@ DATABASES = {
     }
 }
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- reflect-backend/reflect/reflect/urls.py
+++ reflect-backend/reflect/reflect/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 def root_view(request):
     return JsonResponse({"message": "Welcome to the Re-Flect API!"})
@@ -28,3 +30,6 @@ urlpatterns = [
     path("api/users/", include("users.urls")),
     path("api/journal/", include("journal.urls")),
 ]
+
+if settings.DEBUG:  # Only serve media files in development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
