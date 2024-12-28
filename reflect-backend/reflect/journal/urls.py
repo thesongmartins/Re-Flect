9c9eb7216ed24531edcd29d2febe3435903b36cc
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-12-28 14:56:51 +0000

    add the appropriate routes to the url file

--- reflect-backend/reflect/journal/urls.py
+++ reflect-backend/reflect/journal/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import JournalEntryViewSet, MediaViewSet, TagViewSet, MoodViewSet
+from .views import JournalEntryViewSet, TagViewSet, MoodViewSet
 
 router = DefaultRouter()
-router.register(r'entries', JournalEntryViewSet, basename='journalentry')
-router.register(r'media', MediaViewSet, basename='media')
-router.register(r'tags', TagViewSet, basename='tag')
-router.register(r'moods', MoodViewSet, basename='mood')
+router.register(r'entries', JournalEntryViewSet, basename='journal-entries')
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'moods', MoodViewSet, basename='moods')
 
 urlpatterns = [
     path('', include(router.urls)),
