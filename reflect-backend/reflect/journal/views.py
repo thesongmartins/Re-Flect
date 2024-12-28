e03759f0c8b40db2542b91313750816d3c86f462
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2024-12-28 14:54:53 +0000

    correct views to align with the new model and serializers

--- reflect-backend/reflect/journal/views.py
+++ reflect-backend/reflect/journal/views.py
@@ -1,31 +1,32 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import JournalEntry, Media, Tag, Mood
-from .serializers import JournalEntrySerializer, MediaSerializer, TagSerializer, MoodSerializer
-from .utils import analyze_sentiment
+from .models import JournalEntry, Tag, Mood
+from .serializers import JournalEntrySerializer, TagSerializer, MoodSerializer
 
 class JournalEntryViewSet(viewsets.ModelViewSet):
     queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
     permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        journal_entry = serializer.save(user=self.request.user)
-
-        # Perform sentiment analysis using Hugging Face
-        sentiment = analyze_sentiment(journal_entry.content)
-        journal_entry.sentiment_label = sentiment['label']
-        journal_entry.sentiment_score = sentiment['score']
-        journal_entry.save()
-
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
 
 class MoodViewSet(viewsets.ModelViewSet):
     queryset = Mood.objects.all()
     serializer_class = MoodSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(journal_entry__user=self.request.user)
