b5d59f082a30ad0b7ccc20ec1363016a34148866
thesongmartins <songmartins01@gmail.com>
2025-01-15 13:50:02 +0100

    Improving landing page Icons

--- src/data/feauture.js
+++ src/data/feauture.js
@@ -1,34 +1,31 @@
-import heroImg from "../assets/hero-img.svg";
-import graph from '../assets/graph.jpg';
-import mood from '../assets/mood.jpg'
-import voice from '../assets/voice.jpg'
-import upload from '../assets/upload.svg'
+import graph from "../assets/graph.jpg";
+import mood from "../assets/mood.jpg";
+import voice from "../assets/voice.jpg";
+import upload from "../assets/upload.svg";
 
-export const features = [{
+export const features = [
+  {
     id: 1,
     title: "Voice Journaling",
     description: "Share your heart without lifting a finger",
-    img: voice
-
-},
-{
+    img: voice,
+  },
+  {
     id: 2,
     title: "Media Upload",
     description: "Paint a vivid picture of your experience",
-    img: upload
-
-},
-{
+    img: upload,
+  },
+  {
     id: 3,
     title: "Mood Tracker",
     description: "Understand your emotional pattern and find balance",
-    img: mood
-
-},
-{
+    img: mood,
+  },
+  {
     id: 4,
     title: "Timeline View",
     description: "Witness your growth and transformation",
-    img: graph
-
-}]
+    img: graph,
+  },
+];
