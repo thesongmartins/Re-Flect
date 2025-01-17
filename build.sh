8f29edc6f1846e071a62c0390714f423cce63e67
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2025-01-17 16:48:46 +0000

    update build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 pip install -r requirements.txt
 
 # Collect static files and run migrations
-python Re-Flect/reflect-backend/reflect/manage.py collectstatic --no-input
-python Re-Flect/reflect-backend/reflect/manage.py migrate
+python reflect-backend/reflect/manage.py collectstatic --no-input
+python reflect-backend/reflect/manage.py migrate
