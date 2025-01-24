13b93c5baf966d49268a630aa540ffab4cbd1186
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2025-01-24 06:42:48 +0000

    create superuser and link each other

--- reflect-backend/reflect/journal/admin.py
+++ reflect-backend/reflect/journal/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import JournalEntry, Tag, EntryTag, Mood, MoodType, CalendarView
 
-# Register your models here.
+class JournalEntryInline(admin.TabularInline):  # or admin.StackedInline for a more detailed layout
+    model = JournalEntry
+    extra = 0  # Do not display empty forms for new journal entries
+    fields = ('title', 'content', 'photo_url', 'video_url', 'audio_file', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')  # Make timestamp fields read-only
+
+class JournalEntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'updated_at')
+    search_fields = ('title', 'content', 'user__username')
+    list_filter = ('created_at', 'updated_at')
+
+admin.site.register(JournalEntry, JournalEntryAdmin)
+admin.site.register(Tag)
+admin.site.register(EntryTag)
+admin.site.register(Mood)
+admin.site.register(MoodType)
+admin.site.register(CalendarView)
--- reflect-backend/reflect/users/admin.py
+++ reflect-backend/reflect/users/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from journal.models import JournalEntry
 
-# Register your models here.
+
+class JournalEntryInline(admin.TabularInline):
+    model = JournalEntry
+    extra = 0
+    fields = ('title', 'content', 'photo_url', 'video_url', 'audio_file', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email', 'bio', 'profile_picture', 'is_active', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    
+    # Override the fieldsets to exclude `first_name` and `last_name`
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    # Override the add_fieldsets for creating users
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
+    inlines = [JournalEntryInline]
